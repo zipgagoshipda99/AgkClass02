a77f184cb129ea19de81c07beaa9be8e2980c695
Grayson <broo28077@gmail.com>
2026-04-12 13:17:29 +0900

    commit

--- Ineed$$/Assets/Scripts/Inv_Assignment.cs
+++ Ineed$$/Assets/Scripts/Inv_Assignment.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Inv_Assignment : MonoBehaviour
 {
+    public Dictionary<string, int> 
+    inv = new Dictionary<string, int>();
+    
     // Start is called before the first frame update
     void Start()
     {
